e032565c694dcd277dfe2a55653d0023f83874a3
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-12-03 17:55:13 +0000

    Remove unused read perm
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- scripts/build.ts
+++ scripts/build.ts
@@ -38,7 +38,7 @@ const buildFor = async (target: typeof targets[number]) => {
     ? `zip -j ${finalCompressPath} ${finalBinPath}`
     : `tar -czvf ${finalCompressPath} -C ${finalBinDir} ${finalBinName}`;
 
-  await $`deno compile --allow-read=./data --include ./data/quotes.yaml --target=${target} --output ${
+  await $`deno compile --include ./data/quotes.yaml --target=${target} --output ${
     $.path(finalBinPath)
   } main.ts`;
   await $.raw`${compressCmd}`;
