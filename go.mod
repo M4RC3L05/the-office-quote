83ff8f2250a2586dc06030154e02f95a5cb66145
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2025-05-14 22:57:38 +0100

    Convert to golang
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/m4rc3l05/the-office-quote
+
+go 1.24.3
+
+require github.com/goccy/go-yaml v1.17.1
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+
+	"github.com/goccy/go-yaml"
+)
+
+//go:embed data/quotes.yaml
+var quotes []byte
+
+var Version string = "v1.0.12"
+
+type Quote struct {
+	Text string
+}
+
+func main() {
+	flag.CommandLine.SetOutput(os.Stdout)
+
+	helpFlag := flag.Bool("help", false, "Show help menu")
+	versionFlag := flag.Bool("version", false, "Show version")
+
+	flag.Usage = func() {
+		fmt.Printf("The Office Quote\n\n")
+		fmt.Printf("Usage %s [OPTIONS]\n\n", os.Args[0])
+		fmt.Printf("Options:\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *helpFlag {
+		flag.Usage()
+		return
+	}
+
+	if *versionFlag {
+		println(Version)
+		return
+	}
+
+	var data []Quote
+
+	if err := yaml.Unmarshal(quotes, &data); err != nil {
+		panic(err)
+	}
+
+	println(data[rand.Intn(len(data))].Text)
+}
